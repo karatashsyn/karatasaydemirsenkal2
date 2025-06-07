7bd1d2d4f40f04353a6d018fb1d65a7d19470e63
furkansenkal <72168386+furkansenkal@users.noreply.github.com>
2025-06-07 17:16:15 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const diamondAddress = "0xE8E83992B0f23B63e1808419eC6Fe3f47EFd1D7b"; // Your contract address
+export const diamondAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"; // Your contract address
 
-export const tlAddress = "0x3c8190143ba087d06f569d7d0F6E254183F580A2";
+export const tlAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
